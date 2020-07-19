0c61569a6119800dd051574077567abcb7f49e3d
Dillon Long <dplong@crimson.ua.edu>
2020-07-19 14:22:09 -0700

    Implemented displaying the category titles and the point values of each clue in the html game board.

--- index.js
+++ index.js
@@ -1,32 +1,83 @@
 window.addEventListener("DOMContentLoaded",async () => {
-  //getCategories()
   let categoryObjs = new Array(6);
   let clueObjs = new Array();
   categoryObjs = await fetchCategories();
+  console.log(categoryObjs);
   for(let i = 0; i < 6; i++){
     let catId = categoryObjs[i].id;
     let innerArr = await fetchClues(catId);
     clueObjs.push(innerArr);
   }
-  console.log(categoryObjs,clueObjs);
+  // console.log(clueObjs);
+  generateVisual(categoryObjs, clueObjs);
 });
 
 async function fetchCategories(){
-  let res = await fetch('https://jservice.io/api/categories/?count=6');
-  let data = await res.json();
-  return data;
+  const catObjs = [];
+  for(let i = 0; i < 6; i++){
+    let catObj = await fetchCategory(i + 107);
+    // console.log(catObj, ...catObj);
+    catObjs.push(...catObj);
+  }
+  return catObjs;
+}
+
+async function fetchCategory(num){
+  let res = await fetch(`https://jservice.io/api/categories/?offset=${num}`);
+  if(res.ok){
+    let data = await res.json();
+    // console.log(data);
+    return data;
+  } else {
+    handleError();
+  }
 }
 
 async function fetchClues(catId){
   let res = await fetch(`https://jservice.io/api/clues/?category=${catId}`);
-  let data = await res.json();
-  if(data.length === 5) return data;
-  if(data.length > 5){
-    return data.filter((_,i) => {
-      return i % 2 === 1
-    })
+  if(res.ok){
+    let data = await res.json();
+    if(data.length === 5) return data;
+    if(data.length > 5){
+      console.log(data);
+      return data.filter((_,i) => {
+        return i % 2 === 1
+      })
+    } else {
+      handleError();
+    }
   } else {
     handleError();
+  }
+}
+
+function generateVisual(catObjs, clueObjs){
+  generateCatVisual(catObjs);
+  for(let i = 0; i < catObjs.length; i++){
+    generateClueVisual(i, clueObjs);
+  }
+  // for(let i = 0; i < 1; i++){
+  //   generateClueVisual(i, clueObjs);
+  // }
+}
+
+function generateCatVisual(catObjs){
+  // console.log(catObjs);
+  let catEles = document.querySelectorAll('.category');
+  for(let i = 0; i < catObjs.length; i++){
+    // console.log(catEles[i], catObjsList[i]);
+    catEles[i].innerText = catObjs[i].title;
+  }
+}
+
+function generateClueVisual(index, clueObjs){
+  console.log(clueObjs[index]);
+  let columnEle = document.querySelector(`[data-column-num="${index}"]`);
+  let clueEles = columnEle.children;
+  // console.log(columnEle, clueEles, clueObjs);
+  for(let i = 0; i < clueObjs[index].length; i++){
+    // console.log(clueObjs[i]);
+    clueEles[i].innerText = `$${clueObjs[index][i].value}`;
   }
 }
 
